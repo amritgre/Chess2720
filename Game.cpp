ecf08ad96cb00b6d7cdfccbdfccba099a6a29622
Amrit <amritgre@yahoo.com>
2015-09-30 00:55:56 -0400

    Initial setup

---
+++ Game.cpp
@@ -0,0 +1,10 @@
+/*
+ * Game.cpp
+ *
+ *  Created on: Sep 29, 2015
+ *      Author: root
+ */
+
+
+
+
---
+++ Game.h
@@ -0,0 +1,15 @@
+/*
+ * Game.h
+ *
+ *  Created on: Sep 29, 2015
+ *      Author: root
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+
+
+
+
+#endif /* GAME_H_ */
