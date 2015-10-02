7de3c4921c8122e60abbcee67db76a5ce228cdec
Amrit <amritgre@yahoo.com>
2015-10-02 09:34:25 -0400

    Added Chess2720 driver class

---
+++ Chess2720.cpp
@@ -0,0 +1,13 @@
+/*
+ * Chess2720.cpp
+ *
+ *  Created on: Oct 1, 2015
+ *      Author: root
+ */
+#include "Chess.h"
+#include "Chess2720.h"
+
+int main(){
+	Chess* chess = new Chess();
+	return 0;
+}
---
+++ Chess2720.h
@@ -0,0 +1,18 @@
+/*
+ * Chess2720.h
+ *
+ *  Created on: Oct 1, 2015
+ *      Author: root
+ */
+
+#ifndef CHESS2720_H_
+#define CHESS2720_H_
+#define
+
+
+class Chess2720 {
+	public:
+	int main();
+};
+
+#endif /* CHESS2720_H_ */
